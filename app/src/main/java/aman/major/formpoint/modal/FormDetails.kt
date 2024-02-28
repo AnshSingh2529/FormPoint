bdefd8ab322c5b5fea03617e208c709a237144f2
Vibhav <vibhavtiwari6743@gmail.com>
2024-02-28 20:51:52 +0530

    28 feb update

---
+++ app/src/main/java/aman/major/formpoint/modal/FormDetails.kt
@@ -0,0 +1,15 @@
+package aman.major.formpoint.modal
+
+data class FormDetails(
+    val charges: String,
+    val created_at: String,
+    val eligibility: String,
+    val extra_charges: String,
+    val id: String,
+    val level: String,
+    val name: String,
+    val requirements: String,
+    val status: String,
+    val type: String,
+    val updated_at: String
+)
